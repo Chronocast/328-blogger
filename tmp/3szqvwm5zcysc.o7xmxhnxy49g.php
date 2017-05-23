853dc25ccddcb77f738b876bcdbae2c67f1f36a8
Nate Hascup <nshascup@gmail.com>
2017-05-22 19:59:48 -0500

    completed majority of bootstrap theme removal, tweaking vanilla bootstrap to fit assignment parameters

--- tmp/3szqvwm5zcysc.o7xmxhnxy49g.php
+++ tmp/3szqvwm5zcysc.o7xmxhnxy49g.php
@@ -1,47 +1,33 @@
-
 		
-		<?php echo $this->render('pages/header.html',NULL,get_defined_vars(),0); ?>
-		
-		<!---start-content---->
-		<div class="content">
-			<div class="wrap">
-			 <div id="main" role="main">
-			      <ul id="tiles">
-					
-					
-			        <!-- These are our grid blocks -->
-					<?php foreach (($recentBlogs?:[]) as $blog): ?> 
-									<li>
-										<form action='./bloggerPage' method='POST'>
-											<img src='images/Roy.jpg' width='282' height='333'>
-											<div class='post-info'>
-												<div class='post-basic-info'>
-													<input type='hidden' name='blogger' value='<?= $blog['username'] ?>' />
-													<h3><a href='#'><?= $blog['username'] ?></a></h3>
-													<span><input class='form-control btn btn-default' type='submit' value='View my blogs!'></span>
-													<p>Something from my latest blog: <?= $blog['firstLine'] ?></p>
-												</div>
-												<div class='post-info-rate-share'>
-													<div>
-														<span> </span>
-													</div>
-													<div class='post-share'>
-														<span> </span>
-													</div>
-													<div class='clear'> </div>
-												</div>
-											</div>
-										</form>
-									</li>
-										
-									<!-- end block -->
-					<?php endforeach; ?>
-			        <!-- End of grid blocks -->
-			      </ul>
-			    </div>
+				<?php echo $this->render('pages/header.html',NULL,get_defined_vars(),0); ?>
+			
+				<!-- These are our grid blocks 	---->
+				<div class="col-sm-9 wrapper">
+					<div class="container">
+						<div class="row">
+							<div class="content">
+								<!-- begin block -->
+								<?php foreach (($recentBlogs?:[]) as $blog): ?>
+									<div class="col-sm-3">
+										<div class="align-center">
+											<form action='./bloggerPage' method='POST'>
+												<img src='images/Roy.jpg' width='282' height='333'>
+												<input type='hidden' name='blogger' value='<?= $blog['username'] ?>' />
+												<h3><a href='#'><?= $blog['username'] ?></a></h3>
+												<span><input class='btn btn-default' type='submit' value='View my blogs!'></span>
+												<p>Something from my latest blog: <?= $blog['firstLine'] ?></p>
+											</form>
+										</div>
+									</div>
+								<?php endforeach; ?>
+								<!-- end block -->
+							</div>
+						</div>	
+					</div>
+				</div>
+				<!-- End of grid blocks -->
 			</div>
 		</div>
-		<!---//End-content---->
 		
 		<?php echo $this->render('pages/footer.html',NULL,get_defined_vars(),0); ?>
 		
